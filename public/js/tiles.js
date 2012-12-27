93d4294faaa0051fcd1ec1f26170512fc63c91df
Johan Adriaans <johan@HAL2K.lan>
2012-12-27 16:14:09 +0100

    Improved tiles

--- public/js/tiles.js
+++ public/js/tiles.js
@@ -5,31 +5,41 @@ $(function(){
     "http://images.memorix.nl/rce/thumb/400x400/3b24017a-e7ed-c2dd-2452-207db67dc03e.jpg"
   ];
 
+  /* Config */
   var canvas = $("#tiles");
-  var tilesize = 20;
+  var xtiles = 3;
+  var ytiles = 4;
+
+  /* No touchy */
   var tiles = [];
+  var xtilesize = 100/xtiles;
+  var ytilesize = 100/ytiles
 
   function fill ()
   {
+    tiles = [];
+    canvas.empty();
+
     var w = canvas.width();
     var h = canvas.height();
 
     var current = {x:0,y:0};
 
-    for( var index = 0; index < Math.pow(100/tilesize, 2); index++ ) {
-      var ratio = 100/tilesize;
-      var xpos = (index%ratio) * tilesize;
-      var ypos = Math.floor(index/ratio) * tilesize;
+    for( var index = 0; index < ((100/xtilesize)*(100/ytilesize)); index++ ) {
+      var xpos = index%(100/xtilesize) * xtilesize;
+      var ypos = Math.floor(index/(100/xtilesize)) * ytilesize;
       renderTile( xpos, ypos );
     }
   }
+
+  setInterval( function () { fill(); }, 10000 );
 
   setInterval( function () {
     var tile = tiles[Math.floor(Math.random()*tiles.length)];
     var url = images[Math.floor(Math.random()*images.length)];
     tile.fadeTo( 100, 0, function () {
       tile.css( "background", "url("+ url +") no-repeat 50% 50%" );
-      tile.fadeTo( 500, 1 );
+      tile.fadeTo( 700, 1 );
     } );
   }, 2000 );
 
@@ -42,8 +52,8 @@ $(function(){
       position: "absolute",
       top: ypos + "%",
       left: xpos + "%",
-      width: tilesize + "%",
-      height: tilesize + "%"
+      width: xtilesize + "%",
+      height: ytilesize + "%"
     } );
     tiles.push( img );
     canvas.append(img);
