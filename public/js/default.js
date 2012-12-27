f6f38cffaef2f6b5d32831cef4291d0b75243173
jvandereijk <jvandereijk@gmail.com>
2012-12-27 19:03:46 +0100

    Update public/js/default.js

--- public/js/default.js
+++ public/js/default.js
@@ -1,16 +1,26 @@
-﻿$(function () {
+$(function () {
     $("#register").fadeIn(500);
     $("#tiles").bind("select", function (ev, url) {
-        if ($("#TileDialog").is(':visible')) {
-            $("#TileDialog").hide();;
+        if (!$("#register").is(':visible')) {
+            if ($("#TileDialog").is(':visible')) {
+                $("#TileDialog").hide();;
+            }
+            if ($("#StoryDialog").is(':visible')) {
+                $("#StoryDialog").hide();;
+            }
+            showAddStoryDialog(ev, url);
         }
-        showAddStoryDialog(ev, url);
+    });
+    $("#tiles").bind("over", function (ev, url) {
+       // alert($(ev.target).position());
     });
 });
 
 
 function register() {
     $("#userName").text($("#registerForm input[name=name]").val());
+    $("#tiles").trigger("search", $("#registerForm input[name=birth-place]").val());
+    //alert($("#registerForm input[name=birth-place]").val());
     $("#register").fadeOut(500);
     $("#account").show();
 
@@ -29,15 +39,32 @@ function showAddStoryDialog(ev, url) {
 }
 
 function submitStory() {
-    $("#TileDialog").hide();
-    $("#stories nostoriesyet").remove();
+    //$("#TileDialog").hide();
+    $("#stories .story-none").remove();
     var storyDate = $("#storyForm input[name=story-day]").val() + "/" + $("#storyForm input[name=story-month]").val() +
         "/" + $("#storyForm input[name=story-year]").val() + " ";
-    var $story = $("<li>");
-    $story.append($("<div>").text($("#storyForm [name=title]").val()+" S").addClass("story-title"));
-    $story.append($("<div>").text($("#storyForm textarea").val() + " ").addClass("story-main"));
-    $story.append($("<div>").html($("#userName").text() + " - " + storyDate ).addClass("story-author").addClass("pull-right"));
+    var $story = $("<a href='javascript:;'>");
+    $story.append($("<div>").text($("#storyForm [name=title]").val()+" ").addClass("story-title"));
+    //$story.append($("<div>").text($("#storyForm textarea").val() + " ").addClass("hide"));
+    $story.append($("<div>").html($("#userName").text() + " - " + storyDate).addClass("story-author").addClass("pull-right"));
+    $story.click(function () {
+        $("#StoryDialog .story-title").text($("#storyForm [name=title]").val() + " ");
+        $("#StoryDialog .story-main").text($("#storyForm textarea").val() + " ");
+        $("#StoryDialog .story-author").html($("#userName").text() + " - " + storyDate).addClass("story-author").addClass("pull-right");
+        $("#TileDialog").hide();
+        $("#StoryDialog").show();
+    });
+
     $("#stories").append($story);
     
     $("#tiles").trigger("refresh");
+}
+
+function closeTileDialog() {
+    $('#TileDialog').hide();
+}
+
+function closeStoryDialog() {
+    $('#StoryDialog').hide();
+    $('#TileDialog').show();
 }
